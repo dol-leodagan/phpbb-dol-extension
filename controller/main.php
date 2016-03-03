181a02b0a1afb880f0e30b7720442c5e9100a623
dol-leodagan <leodagan-github@durron.net>
2016-03-03 22:05:37 +0100

    Create main.php

---
+++ controller/main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*
+* @package DOL Extension 0.0.1
+* @copyright (c) 2016 Leodagan
+* @license MIT
+*
+*/
+namespace dol\status\controller;
+class main
+{
+    public function __construct()
+    {
+    }
+    
+    public function handle()
+    {
+    }
+}
