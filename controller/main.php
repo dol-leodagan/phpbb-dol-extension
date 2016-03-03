cb177123e3d24a36e449fc5304d0c9ec15e351e9
dol-leodagan <leodagan-github@durron.net>
2016-03-03 22:10:58 +0100

    Update main.php

--- controller/main.php
+++ controller/main.php
@@ -9,9 +9,23 @@
 namespace dol\status\controller;
 class main
 {
-    public function __construct()
+    /**
+    * PHP file extension
+    * @var string
+    */
+    protected $php_ext;
+    /**
+    * Portal root path
+    * @var string
+    */
+    protected $root_path;
+
+    public function __construct($phpbb_root_path, $php_ext)
     {
-    }
+        $this->phpbb_root_path = $phpbb_root_path;
+        $this->php_ext = $php_ext;
+        $this->root_path = $phpbb_root_path . 'ext/dol/status/';
+}
     
     public function handle()
     {
