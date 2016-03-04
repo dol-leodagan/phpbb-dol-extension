8c7316270f735f110037978afb0bef9f933c7072
kypdurron <kyp@durron.net>
2016-03-04 22:24:15 +0100

    Update: improve controller to use template

--- controller/main.php
+++ controller/main.php
@@ -7,14 +7,33 @@
 *
 */
 namespace dol\status\controller;
+
+use phpbb\config\config;
+use phpbb\controller\helper;
+use phpbb\template\template;
+use phpbb\user;
+use Symfony\Component\HttpFoundation\Response;
+
 class main
 {
+     /* @var config */
+    protected $config;
+
+    /* @var helper */
+    protected $helper;
+
+    /* @var template */
+    protected $template;
+
+    /* @var user */
+    protected $user;
+
     /**
     * phpBB root path
     * @var string
     */
     protected $phpbb_root_path;
-	
+    
     /**
     * PHP file extension
     * @var string
@@ -26,8 +45,22 @@ class main
     */
     protected $root_path;
 
-    public function __construct($phpbb_root_path, $php_ext)
+    /**
+     * Constructor
+     *
+     * @param config $config
+     * @param helper $helper
+     * @param template $template
+     * @param user $user
+     * @param string $phpbb_root_path
+     * @param string $php_ext
+     */
+    public function __construct(config $config, helper $helper, template $template, user $user, $phpbb_root_path, $php_ext)
     {
+        $this->config = $config;
+        $this->helper = $helper;
+        $this->template = $template;
+        $this->user = $user;
         $this->phpbb_root_path = $phpbb_root_path;
         $this->php_ext = $php_ext;
         $this->root_path = $phpbb_root_path . 'ext/dol/status/';
@@ -35,5 +68,6 @@ class main
     
     public function handle()
     {
+        return $this->helper->render('herald_body.html', $name);
     }
 }
